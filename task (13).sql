6361e512c99fb262e676d4869adbaf510500b2f9
Joanna Konopacka <86154250+joanna-konopacka@users.noreply.github.com>
2023-06-21 22:53:34 +0200

    task13

--- task (13).sql
+++ task (13).sql
@@ -2,9 +2,7 @@
 The category and product tables are given.
 
 Find the number of products that belong to the 'Meat' or 'Grains' categories.
-
 The result should contain the following columns:
-
 category_name is the category name;
 number_of_products is the number of products.
 Please note: do not forget to group the result.
@@ -19,11 +17,13 @@ __________________________product_____________________
 | id | name        | amount   | price   | category_id |
 | 2  | Shrimp      | 50       | 450     | 2           |
 .......................................................
-| 8  | Flour       | 20       | 60      | 4    
-*/
+| 8  | Flour       | 20       | 60      | 4    */
 
-SELECT category.name AS category_name, COUNT(*) AS number_of_products
-FROM product
-INNER JOIN category ON category.id = product.category_id
-WHERE category.name IN ('Meat', 'Grains')
-GROUP BY category.name;
+SELECT c.name AS category_name, COUNT (*) AS number_of_products
+FROM category
+INNER JOIN product
+ON c.id = p.category_id
+WHERE c.name IN ('Meat', 'Grains')
+GROUP BY category_name
+
+
